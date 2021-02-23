3f0815cd5a10ff4bac9c2d5f479dc6e8bef0e190
Alazar Kassahun <alazar.kas@gmail.com>
2021-02-23 17:30:10 +0300

    optimizing initialization

--- app/Http/Controllers/InitializeVoteController.php
+++ app/Http/Controllers/InitializeVoteController.php
@@ -43,12 +43,13 @@ class InitializeVoteController extends Controller
                 //     $meetingAgenda->korem += $attendedDelegate->no_of_shares;
                 // }
                 
-                // foreach ($attendedShareholders as $attendedShareholder ) {
-                //     $meetingAgenda->yes += $attendedShareholder->no_of_shares;
-                //     $meetingAgenda->korem += $attendedShareholder->no_of_shares;
+                foreach ($attendedShareholders as $attendedShareholder ) {
+                    // $meetingAgenda->yes += $attendedShareholder->no_of_shares;
+                    // $meetingAgenda->korem += $attendedShareholder->no_of_shares;
+                    $attendedShareholder->meetingAgendas()->attach($meetingAgenda, ['answer' => 'እደግፋለሁ', 'user_id' => 0]);
                     
-                // }
-                $meetingAgenda->shareHolders()->attach($attendedShareholders, ['answer' => 'እደግፋለሁ', 'user_id' => 0]);
+                }
+                // $meetingAgenda->shareHolders()->attach($attendedShareholders, ['answer' => 'እደግፋለሁ', 'user_id' => 0]);
                 $meetingAgenda->save();
                 return response()->json([
                     'success' => true
